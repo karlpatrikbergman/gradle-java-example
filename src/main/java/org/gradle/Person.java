e370dc23880ff3434028af0b4552c71235c82473
Patrik Bergman <patrik.bergman@transmode.com>
2015-09-22 13:37:00 +0200

    Fixed checkstyle errors

--- src/main/java/org/gradle/Person.java
+++ src/main/java/org/gradle/Person.java
@@ -5,12 +5,12 @@ import org.apache.commons.collections.list.GrowthList;
 public class Person {
     private final String name;
 
-    public Person(String name) {
+    public Person(final String name) {
         this.name = name;
         new GrowthList();
     }
 
-    public String getName() {
+    public final String getName() {
         return name;
     }
 }
--- src/test/java/org/gradle/PersonTest.java
+++ src/test/java/org/gradle/PersonTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 public class PersonTest {
     @Test
-    public void canConstructAPersonWithAName() {
+    public final void canConstructAPersonWithAName() {
         Person person = new Person("Larry");
         assertEquals("Larry", person.getName());
     }
